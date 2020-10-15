ecb3148197239965c43b7ead763a09261fff301b
Nobuharu Murata <mrtnbhr@gmail.com>
2020-10-15 20:23:10 +0900

    1-1

---
+++ index.php
@@ -0,0 +1,13 @@
+//htdocs/htmlTest/index.php
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>はじめてのwebサイトへようこそ</p>
+  </body>
+</html>
